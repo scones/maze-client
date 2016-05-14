8a8abfdc96996de9f88f63a28e313faccad91f01
Dirk Gustke <code+git@asm68k.org>
2016-05-14 10:23:14 -0500

    master - removed database

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,14 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+#require 'active_record'
+require 'action_controller/railtie'
+require       'action_view/railtie'
+require     'action_mailer/railtie'
+require        'active_job/railtie'
+#require 'rails/test_unit'
+#require 'sprockets'
+
+#require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,6 +29,6 @@ module Maze
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+#    config.active_record.raise_in_transactional_callbacks = true
   end
 end
