b68d90d1a853af78ea9fcac9acd22909c5246520
Dirk Gustke <code+git@asm68k.org>
2016-05-15 04:40:45 -0500

    master - added three.js and jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+// require jquery
+// require jquery_ujs
+// require turbolinks
+// require_tree .
+
+//= require 'three-0.76.0'
+//= require 'jquery-2.2.3'
+
