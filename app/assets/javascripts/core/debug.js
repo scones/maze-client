b8a3a1e8b1dda34ba148345544ccc5c82f4ec2f6
Dirk Gustke <code+git@asm68k.org>
2016-05-23 08:45:43 -0500

    master - added method inspection and namespace

--- app/assets/javascripts/core/debug.js
+++ app/assets/javascripts/core/debug.js
@@ -1,49 +1,57 @@
 
 core = core || {};
+core.debug = core.debug || {};
+core.debug.console = core.debug.console || {};
 
-core.DEBUG_LEVEL = {
+core.debug.DEBUG_LEVEL = {
   NO_DEBUG: 0,
   ERROR: 10,
   WARN: 20,
   LOG: 30,
   INFO: 40
 }
+core.debug.level = core.debug.level || core.debug.DEBUG_LEVEL.NO_DEBUG;
 
-core.debug = core.debug || core.DEBUG_LEVEL.NO_DEBUG;
+core.debug.errors = [];
+core.debug.warnings = [];
+core.debug.logs = [];
+core.debug.infos = [];
 
-core.errors = [];
-core.warnings = [];
-core.logs = [];
-core.infos = [];
+core.debug.console_pure = window.console;
 
-core.console_pure = window.console;
 
-core.console = {};
-core.console.error = function(message) {
-  core.errors.push(message);
-  if (core.debug >= core.DEBUG_LEVEL.ERROR)  {
-    core.console_pure.error(message);
+core.debug.console.error = function(message) {
+  core.debug.errors.push(message);
+  if (core.debug.level >= core.debug.DEBUG_LEVEL.ERROR)  {
+    core.debug.console_pure.error(message);
   }
-}
-core.console.warn = function(message) {
-  core.warnings.push(message);
-  if (core.debug >= core.DEBUG_LEVEL.WARN) {
-    core.console_pure.warn(message);
+};
+core.debug.console.warn = function(message) {
+  core.debug.warnings.push(message);
+  if (core.debug.level >= core.debug.DEBUG_LEVEL.WARN) {
+    core.debug.console_pure.warn(message);
   }
-}
-core.console.log = function(message) {
-  core.logs.push(message);
-  if (core.debug >= core.DEBUG_LEVEL.LOG) {
-    core.console_pure.log(message);
+};
+core.debug.console.log = function(message) {
+  core.debug.logs.push(message);
+  if (core.debug.level >= core.debug.DEBUG_LEVEL.LOG) {
+    core.debug.console_pure.log(message);
   }
-}
-core.console.info = function(message) {
-  core.infos.push(message);
-  if (core.debug >= core.DEBUG_LEVEL.INFO) {
-    core.console_pure.info(message);
+};
+core.debug.console.info = function(message) {
+  core.debug.infos.push(message);
+  if (core.debug.level >= core.debug.DEBUG_LEVEL.INFO) {
+    core.debug.console_pure.info(message);
   }
-}
+};
 
-console = core.console;
+console = core.debug.console;
 window.console = console;
+
+
+core.debug.print_methods = function(object) {
+  console.log(Object.getOwnPropertyNames(object).filter(function (p) {
+    return typeof object[p] === 'function';
+  }));
+};
 
